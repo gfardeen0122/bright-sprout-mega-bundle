e4f4a46d26de08a5de351c488507c9e2b96c2cfa
gfardeen0122 <gfardeen0122@gmail.com>
2025-11-05 01:20:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: "./", // 👈 This is the key line for cPanel or subdomain hosting
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  base: "./", // <-- crucial for cPanel static hosting
+});
